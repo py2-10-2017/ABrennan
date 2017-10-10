a4a3b364a0877abd80aa821939b151304d8c730a
Angela Brennan <brennan.angela.m@gmail.com>
2017-10-10 00:14:10 -0500

    Remove products from session and breakout in new file

---
+++ django/amadon/apps/checkout/products.py
@@ -0,0 +1,12 @@
+items = [
+    { 'name': 'Openfan', 'price': 5.99 },
+    { 'name': 'Flexsoft', 'price': 12.99 },
+    { 'name': 'Rehome', 'price': 3.16 },
+    { 'name': 'Vollux', 'price': 4.00 },
+    { 'name': 'Inch Antam', 'price': 16.27 },
+    { 'name': 'Unanamsoft', 'price': 5.99 },
+    { 'name': 'Tam-Lex', 'price': 2.79 },
+    { 'name': 'Konkin', 'price': 3.31 },
+    { 'name': 'Silver Zamtech', 'price': 7.98 },
+    { 'name': 'Mathtrax', 'price': 2.99 }
+]
--- django/amadon/apps/checkout/views.py
+++ django/amadon/apps/checkout/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from products import items
 
 def index(request):
 
@@ -6,25 +7,11 @@ def index(request):
 
 def products(request):
 
-    products = [
-        { 'name': 'Openfan', 'price': 5.99 },
-        { 'name': 'Flexsoft', 'price': 12.99 },
-        { 'name': 'Rehome', 'price': 3.16 },
-        { 'name': 'Vollux', 'price': 4.00 },
-        { 'name': 'Inch Antam', 'price': 16.27 },
-        { 'name': 'Unanamsoft', 'price': 5.99 },
-        { 'name': 'Tam-Lex', 'price': 2.79 },
-        { 'name': 'Konkin', 'price': 3.31 },
-        { 'name': 'Silver Zamtech', 'price': 7.98 },
-        { 'name': 'Mathtrax', 'price': 2.99 }
-    ]
+    context = {
+        'items': items
+    }
 
-    products = { 'products': products }
-
-    # saving products as variable to use when multiplying in buying process - normally would be database
-    request.session['products'] = products
-
-    return render(request, 'checkout/index.html', products)
+    return render(request, 'checkout/index.html', context)
 
 
 def buy(request):
@@ -39,8 +26,7 @@ def buy(request):
     request.session['purchases'] = temp
 
     p_id = int(request.POST['product_id'])-1
-    price = request.session['products'][request.session['products'].keys()[0]][p_id]
-    price = price['price']
+    price = items[p_id]['price']
 
     try:
         request.session['total']
@@ -65,6 +51,6 @@ def empty(request):
     del request.session['purchases']
     del request.session['newPurchases']
     del request.session['total']
-    del request.session['num']
+    #del request.session['num']
 
     return redirect('/amadon/checkout')
