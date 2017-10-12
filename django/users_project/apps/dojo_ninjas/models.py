88821086fd53f1743718eb223ae722dfa637413d
Angela Brennan <brennan.angela.m@gmail.com>
2017-10-12 17:53:03 -0500

    Adding dojo ninjas app to users_project for dojo ninjas assignment

---
+++ django/users_project/apps/dojo_ninjas/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+from django.db import models
+
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    desc = models.TextField(default='')
+class Ninja(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dojo = models.ForeignKey(Dojo, related_name = "ninjas")
