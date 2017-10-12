59e1e13a3f6768fba8180c3b74d4a957ab050c69
Angela Brennan <brennan.angela.m@gmail.com>
2017-10-12 14:53:55 -0500

    Updating new app assignment to include register and login

--- django/new_app/apps/users/urls.py
+++ django/new_app/apps/users/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index),
+    url(r'^$', views.index),
     url(r'^new/$', views.register),
+    url(r'^register/', views.register),
+    url(r'^login/$', views.login),
 ]
